27ba27a6d03aa1afa1f423959e36a2af16af730f
accounts-google-v3 <rsqi01428@proton.me>
2025-08-29 22:23:58 +0000

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,4 +1,5 @@
-        import { createClient } from '@supabase/supabase-js'
+
+  import { createClient } from 'https://esm.sh/@supabase/supabase-js@2'
         const supabaseUrl = 'https://ypndiydnhvetesszfgwc.supabase.co'
         const supabaseKey = "ZXlKaGJHY2lPaUpJVXpJMU5pSXNJblI1Y0NJNklrcFhWQ0o5LmV5SnBjM01pT2lKemRYQmhZbUZ6WlNJc0luSmxaaUk2SW5sd2JtUnBlV1J1YUhabGRHVnpjM3BtWjNkaklpd2ljbTlzWlNJNkltRnViMjRpTENKcFlYUWlPakUzTlRZMU1ESTFORGNzSW1WNGNDSTZNakEzTWpBM09EVTBOMzAuMG91SG9meTMycFVfc25td2lQVDJDaDFQYTFLWjA3Rk9IX1hSeEVVM3dkOA"
         const supabase = createClient(supabaseUrl, window[atob("YXQ")+atob("b2I")](supabaseKey))
